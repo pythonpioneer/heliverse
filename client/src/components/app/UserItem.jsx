72346cd001a52c186146abdabb987358e84c87e3
Hritik <pypi.pioneer@gmail.com>
2023-11-21 15:47:01 +0530

    icon added to delete the user

--- client/src/components/app/UserItem.jsx
+++ client/src/components/app/UserItem.jsx
@@ -4,10 +4,10 @@ import { Grid } from '@mui/material';
 export default function UserItem(props) {
     return (
         <>
-            <Grid container className="container card mt-4" style={{ width: '90%', height: '180px' }}>
+            <Grid container className="container card mt-4" style={{ width: '90%', height: 'auto', justifyContent: 'space-between', flexWrap: 'wrap' }}>
                 <Grid item container alignItems="center">
                     <img className="mr-4" src={props.user.avatar} style={{ height: '50px' }} alt="avatar" />
-                    
+
                     <Grid style={{ margin: '5px', paddingRight: '50px' }}>
                         <h5>{props.user.first_name + " " + props.user.last_name}</h5>
                         <p>{props.user.gender}</p>
@@ -19,7 +19,9 @@ export default function UserItem(props) {
                     <p style={{ marginTop: '-12px' }}>domain: {props.user.domain}</p>
                     <p style={{ marginTop: '-12px' }}>Availiblity: {props.user.available ? 'Yes' : 'No'}</p>
                 </Grid>
+                <i className="fa fa-trash" style={{ marginTop: '10px' }} aria-hidden="true"></i>
             </Grid>
         </>
+
     )
 }
