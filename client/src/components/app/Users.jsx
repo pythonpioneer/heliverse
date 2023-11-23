2db3b67a3a6c0945a0bbffce314ea0d484083251
Hritik <pypi.pioneer@gmail.com>
2023-11-23 19:47:16 +0530

    infinite scrollable added

--- client/src/components/app/Users.jsx
+++ client/src/components/app/Users.jsx
@@ -3,6 +3,7 @@ import UserItem from './UserItem';
 import { Grid } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../../redux/slice/user';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 // to display all the users
@@ -20,7 +21,7 @@ export default function Users() {
   // to fetch users details
   useEffect(() => {
     dispatch(fetchUsers());
-    return (()=> {});  // will write clening function
+    return (() => { });  // will write clening function
   }, [dispatch]);
 
 
@@ -29,15 +30,28 @@ export default function Users() {
       <Grid container spacing={4}>
 
         {/* fetching data from server */}
-        { loading && <h1 className='container mt-5'>Loading...</h1>}
+        {loading && <h1 className='container mt-5'>Loading...</h1>}
 
-        {/* traversing to display all users */}
-        { !loading && users?.map((user, index) => {
-          return <Grid key={index} item lg={4} xs={12} sm={6} md={4} >
-              <UserItem user={user} />
-            </Grid>
-          })
-        }
+        {/* implementing pagenation */}
+        <InfiniteScroll
+          dataLength={50}
+          next={() => {}}
+          hasMore={true}  // there are alot of articles present, restricting after displaying some articles out of those
+          loader={<h2>Loading...</h2>}
+        >
+          <Grid container>
+
+
+            {/* traversing to display all users */}
+            {!loading && users?.map((user, index) => {
+              return <Grid key={index} item lg={4} xs={12} sm={6} md={4} >
+                <UserItem user={user} />
+              </Grid>
+            })
+            }
+
+          </Grid>
+        </InfiniteScroll>
       </Grid>
     </>
   )
